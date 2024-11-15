31ffd52a5249d169cbbefd52474d27b82033a141
doriangray <d0r1an.gray@yandex.ru>
2024-11-15 14:29:21 +0300

    Homework red and black tree

--- Seminar4/src/main/java/org/gaenkov/Main.java
+++ Seminar4/src/main/java/org/gaenkov/Main.java
@@ -1,31 +1,32 @@
 package org.gaenkov;
 
+import org.gaenkov.homework.Tree;
 import org.gaenkov.zadanie1.HashMap;
-import org.gaenkov.zadanie1.Tree;
 
 import java.util.Random;
 
 public class Main {
     public static void main(String[] args) {
-//        Tree tree = new Tree();
-//        Random rnd = new Random();
-//        for(int i = 1; i <=6; i++){
-//            tree.insert(i);
-//        }
+        Tree tree = new Tree();
+        Random rnd = new Random();
+        for(int i = 1; i <=6; i++){
+            tree.insert(i);
+        }
+
+        System.out.println(tree.find(1));
+        System.out.println(tree.find(6));
+
+//        HashMap map = new HashMap();
+//        map.push(1, 20);
+//        map.push(3, 40);
 //
-//        System.out.println(tree.find(1));
-//        System.out.println(tree.find(6));
-        HashMap map = new HashMap();
-        map.push(1, 20);
-        map.push(3, 40);
-
-        System.out.println(map.find(1));
-        System.out.println(map.find(2));
-
-        map.remove(1);
-        map.push(2, 50);
-
-        System.out.println(map.find(1));
-        System.out.println(map.find(2));
+//        System.out.println(map.find(1));
+//        System.out.println(map.find(2));
+//
+//        map.remove(1);
+//        map.push(2, 50);
+//
+//        System.out.println(map.find(1));
+//        System.out.println(map.find(2));
     }
 }
