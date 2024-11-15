78ff9b1dd8f612841946d877f5119a72d9c4d632
doriangray <d0r1an.gray@yandex.ru>
2024-11-15 13:35:03 +0300

    Realization of balanced red and black tree

--- Seminar4/src/main/java/org/gaenkov/Main.java
+++ Seminar4/src/main/java/org/gaenkov/Main.java
@@ -6,13 +6,13 @@ import java.util.Random;
 
 public class Main {
     public static void main(String[] args) {
-
         Tree tree = new Tree();
-        Random rnd = new Random();
-        for(int i = 1; i <=5; i++)
-            tree.insert(rnd.nextInt(10));
+//        Random rnd = new Random();
+        for(int i = 1; i <=6; i++){
+            tree.insert(i);
+        }
 
-        System.out.println(tree.find(1));
-        System.out.println(tree.find(6));
+//        System.out.println(tree.find(1));
+//        System.out.println(tree.find(6));
     }
 }
--- Seminar4/src/main/java/org/gaenkov/zadanie1/Tree.java
+++ Seminar4/src/main/java/org/gaenkov/zadanie1/Tree.java
@@ -6,14 +6,26 @@ public class Tree {
         int value;
         Node left;
         Node right;
+
+        Color color;
+    }
+
+    enum Color{
+        BLACK,
+        RED
+
     }
 
     public void insert(int value){
-        if(root != null) insert(root, value);
+        if(root != null){
+            insert(root, value);
+            root = balance(root);
+        }
         else{
             root = new Node();
             root.value = value;
         }
+        root.color = Color.BLACK;
     }
 
     private void insert(Node node, int value){
@@ -22,18 +34,22 @@ public class Tree {
                 if(node.right == null){
                     node.right = new Node();
                     node.right.value = value;
+                    node.right.color = Color.RED;
                 }
                 else{
                     insert(node.right, value);
+                    node.right = balance(node.right);
                 }
             }
             else{
                 if(node.left == null){
                     node.left = new Node();
                     node.left.value = value;
+                    node.left.color = Color.RED;
                 }
                 else{
                     insert(node.left, value);
+                    node.left = balance(node.left );
                 }
             }
         }
@@ -49,5 +65,53 @@ public class Tree {
         if(node.value < value) return find(node.right, value);
         else find(node.left, value);
         return null;
+    }
+
+    private Node leftRotate(Node node){
+        Node cur = node.right;
+        node.right = cur.left;
+        cur.left = node;
+        cur.color = node.color;
+        node.color = Color.RED;
+        return cur;
+    }
+
+    private Node rightRotate(Node node){
+        Node cur = node.left;
+        node.left = cur.right;
+        cur.right = node;
+        cur.color = node.color;
+        node.color = Color.RED;
+        return cur;
+    }
+
+    private void swapColors(Node node){
+        node.color = (node.color == Color.RED ? Color.BLACK : Color.RED); //Елси красный цвет меняем на чреный и наоборот
+        node.left.color = Color.BLACK;
+        node.right.color = Color.BLACK;
+    }
+
+    private Node balance(Node node){
+        boolean flag = true;
+        Node cur = node;
+        do{
+            flag = false;
+
+            if(cur.right != null && cur.right.color == Color.RED && (cur.left == null || cur.left.color == Color.BLACK)){
+                cur = leftRotate(cur);
+                flag = true;
+            }
+
+            if(cur.left != null && cur.left.color == Color.RED && cur.left.left != null && cur.left.left.color == Color.RED){
+                cur = rightRotate(cur);
+                flag = true;
+            }
+
+            if(cur.left != null && cur.left.color == Color.RED && cur.right != null && cur.right.color == Color.RED){
+                swapColors(cur);
+                flag = true;
+            }
+        }while(flag);
+        return cur;
     }
 }
